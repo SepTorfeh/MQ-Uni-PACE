48da4f1cca580a864e4d0aaa0f6ddd4a60768dc5
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2022-03-16 11:12:54 +1100

    adding little style to LoginForm

--- app/src/components/login/LoginForm.tsx
+++ app/src/components/login/LoginForm.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { mobileMaxWidth } from '../reusable/screenSize';
 
 const StyledLoginForm = styled.form`
     width: auto;
     height: auto;
     position: relative;
+    justify-self: center;
+
+    @media (max-width: ${mobileMaxWidth}){
+        align-self: center;
+    }
 
 `
 
 const LoginForm = () => {
 
     return (
-            <div>
             <StyledLoginForm>
                 <h1>Welcome</h1>
             </StyledLoginForm>
-            </div>
     )
 
 }
