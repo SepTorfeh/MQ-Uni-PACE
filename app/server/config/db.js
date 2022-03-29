7a3c537d46c8994705a65e50238af9d127ead948
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2022-03-29 20:30:35 +1100

    config file to connect to mogodb

--- app/server/config/db.js
+++ app/server/config/db.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL, {
-            dbName: "pace",
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         if (process.env.NODE_ENV !== "test") {
-            console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+            //not running this for now
+            console.log(`MongoDB Connected: ${conn.connection.host}`);
         }
     } catch (error) {
         if (process.env.NODE_ENV !== "test") {
-            console.error(`Error ${error}`.red.underline.bold);
+            //console.error(`Error ${error}`.red.underline.bold);
+            console.error(`Error ${error}`);
         }
         process.exit(1);
     }
