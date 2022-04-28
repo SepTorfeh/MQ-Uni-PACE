e83e93a057639dd023321b71064a45b375a30d36
Lance W <lance.te@students.mq.edu.au>
2022-04-28 20:32:22 +1000

    validation check on questions

--- app/client/src/components/survey/QuestionWrapper.tsx
+++ app/client/src/components/survey/QuestionWrapper.tsx
@@ -4,12 +4,16 @@ import Navbar from '../reusable/Navbar'
 import QuestionComponent from './QuestionComponent'
 import { useAppSelector } from '../../redux/hooks'
 import { useNavigate } from 'react-router-dom'
-
+import CustomModal from '../reusable/CustomModal'
 
 const QuestionWrapper = () => {
 
     const navigate = useNavigate()
     const isAuthenticated = useAppSelector(state => !!state.login.token)
+
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
 
     useEffect(() => {
         if(!isAuthenticated){
@@ -19,16 +23,18 @@ const QuestionWrapper = () => {
 
     const [data, setData] = useState<any[]>([])
 
+    // Please pass data to the db - Lance
     const submit = () => {
-        // Please connect to db - Lance
+        const validation = !data.some(el => el < 0)
         console.log(data)
-        navigate('/loading')
+        validation ? navigate('/loading') : handleOpen()
     }
 
     return (
         <div className="page">
             <Navbar />
             <div className="container">
+                <CustomModal error state={open} handleClose={handleClose} content="Your answers will help, we promise!" />
                 <QuestionComponent data={setData} />
                 {/*<button type="button" className="button green-button" onClick={() => navigate('/persona')} tabIndex={-1}>Back</button>*/}
                 <button type="button" className="button green-button float-right" onClick={() => submit()} tabIndex={-1}>Submit</button>
