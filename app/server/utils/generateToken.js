aa60aa9b8712e36864d66ea1ead0ca317913e43c
sepehrtorfehnezhad <s.torfeh.75@gmail.com>
2022-05-16 22:32:47 +1000

    set token expiry to 30 mins

--- app/server/utils/generateToken.js
+++ app/server/utils/generateToken.js
@@ -3,11 +3,11 @@ const jwt = require("jsonwebtoken");
 const generateToken = (id) => {
     if (process.env.NODE_ENV === "test") {
         return jwt.sign({id}, "testing", {
-            expiresIn: "30d",
+            expiresIn: 1800,
         });
     } else {
         return jwt.sign({id}, process.env.JWT_SECRET, {
-            expiresIn: "30d",
+            expiresIn: 1800,
         });
     }
 };
